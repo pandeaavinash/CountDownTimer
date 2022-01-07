22d5bc27b3e62945d8a4fc5bd5b5f9ebd1f0b5c2
avinash <aavinashpande@gmail.com>
2022-01-07 17:37:57 +0530

    uncommented code

--- CountDownTimer/src/test/java/com/runner/TestRunner.java
+++ CountDownTimer/src/test/java/com/runner/TestRunner.java
@@ -4,8 +4,8 @@ package com.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
-	@CucumberOptions(features = "src/test/resources/features", glue = "com.countdown.stepdef",tags= "@tensecond")
+	
+	@CucumberOptions(features = "src/test/resources/features", glue = "com.countdown.stepdef",tags= "",  monochrome=true)
 	public class TestRunner extends AbstractTestNGCucumberTests 
 	{
 	 
